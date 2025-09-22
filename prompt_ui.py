426b6a6bb71c1d3a6324c21680749f413a5fb9f1
krishnag8950 <krishnag8950@gmail.com>
2025-09-22 06:40:03 +0000

    Updated project files

--- prompt_ui.py
+++ prompt_ui.py
@@ -4,25 +4,29 @@ import streamlit as st
 from langchain_core.prompts import PromptTemplate,load_prompt
 
 load_dotenv()
-model = ChatGoogleGenerativeAI(model="gemini-1.5-flash")
 
 st.header('Reasearch Tool')
 
-paper_input = st.selectbox( "Select Research Paper Name", ["Attention Is All You Need", "BERT: Pre-training of Deep Bidirectional Transformers", "GPT-3: Language Models are Few-Shot Learners", "Diffusion Models Beat GANs on Image Synthesis"] )
+api_key = st.text_input("Enter your Google API Key:", type="password")
 
-style_input = st.selectbox( "Select Explanation Style", ["Beginner-Friendly", "Technical", "Code-Oriented", "Mathematical"] ) 
+if api_key:
+    model = ChatGoogleGenerativeAI(model="gemini-1.5-flash", google_api_key=api_key)
 
-length_input = st.selectbox( "Select Explanation Length", ["Short (1-2 paragraphs)", "Medium (3-5 paragraphs)", "Long (detailed explanation)"] )
+    paper_input = st.selectbox( "Select Research Paper Name", ["Attention Is All You Need", "BERT: Pre-training of Deep Bidirectional Transformers", "GPT-3: Language Models are Few-Shot Learners", "Diffusion Models Beat GANs on Image Synthesis"] )
 
-template = load_prompt('template.json')
+    style_input = st.selectbox( "Select Explanation Style", ["Beginner-Friendly", "Technical", "Code-Oriented", "Mathematical"] ) 
+
+    length_input = st.selectbox( "Select Explanation Length", ["Short (1-2 paragraphs)", "Medium (3-5 paragraphs)", "Long (detailed explanation)"] )
+
+    template = load_prompt('template.json')
 
 
 
-if st.button('Summarize'):
-    chain = template | model
-    result = chain.invoke({
-        'paper_input':paper_input,
-        'style_input':style_input,
-        'length_input':length_input
-    })
-    st.write(result.content)
+    if st.button('Summarize'):
+        chain = template | model
+        result = chain.invoke({
+            'paper_input':paper_input,
+            'style_input':style_input,
+            'length_input':length_input
+        })
+        st.write(result.content)
